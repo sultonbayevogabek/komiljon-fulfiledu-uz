02df688e061466523b3a8436acbcf6a466f57464
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-06-28 23:08:50 +0500

    commit

--- public/js/excel.js
+++ public/js/excel.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/+oRz1oXGmSVpjYWEy';
+    window.location.href = 'https://t.me/+t1Gm1s8TV1NhYTVi';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
